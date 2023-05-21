bb78647d9ad288bc4ec6dae2a262f8efd855eced
KhAlamdar11 <phoenixdealamdar2@gmail.com>
2023-05-21 23:03:41 +0200

    perception-intevention integrated

--- src/perception_intervention.py
+++ src/perception_intervention.py
@@ -8,27 +8,23 @@ from std_srvs.srv import Trigger, TriggerRequest
 from geometry_msgs.msg import PoseStamped
 
 from autonomous_task_NAK.srv import intervention_getpoint
+from autonomous_task_NAK.srv import MarkerPose
 from std_srvs.srv import SetBool
 
 ###   Move to point behaviour
 class GoToPoint(py_trees.behaviour.Behaviour):
-    def __init__(self, name, pick_or_place, location):
+    def __init__(self, name, pick_or_place):
         super(GoToPoint, self).__init__(name)
 
         # attach to blackboard and allow read access to object name
         self.blackboard = self.attach_blackboard_client(name=self.name)
                 
         # Blackboard variable to keep a track of which pickup location is next
-        # self.blackboard.register_key(
-        #     "location", access=py_trees.common.Access.READ)
-        # self.blackboard.register_key(
-        #     "location", access=py_trees.common.Access.WRITE)
-        # self.blackboard.register_key(
-        #     "pick_or_place", access=py_trees.common.Access.READ)
-        # self.blackboard.register_key(
-        #     "pick_or_place", access=py_trees.common.Access.WRITE)
+        self.blackboard.register_key(
+            "location", access=py_trees.common.Access.READ)
+        self.blackboard.register_key(
+            "location", access=py_trees.common.Access.WRITE)
         
-        self.location = location
         self.pick_or_place = pick_or_place
         
     def setup(self):
@@ -60,14 +56,9 @@ class GoToPoint(py_trees.behaviour.Behaviour):
     def initialise(self):
         self.logger.debug("  %s [GoToPoint::initialise()]" % self.name)
 
-        pose = PoseStamped()
-        pose.pose.position.x = self.location[0]
-        pose.pose.position.y = self.location[1]
-        pose.pose.position.z = self.location[2]
-
-        self.server_set_goal(pose)
+        self.server_set_goal(self.blackboard.location)
         
-        print("curr pickup loc: ", self.location)
+        # print("curr pickup loc: ", self.location)
 
 
     def update(self):
@@ -102,25 +93,83 @@ class GoToPoint(py_trees.behaviour.Behaviour):
                           (self.name, self.status, new_status))
 
 
+class SeeAruco(py_trees.behaviour.Behaviour):
+    def __init__(self, name):
+        super(SeeAruco, self).__init__(name)
+
+        # attach to blackboard and allow read access to object name
+        self.blackboard = self.attach_blackboard_client(name=self.name)
+                
+        # Blackboard variable to keep a track of which pickup location is next
+        self.blackboard.register_key(
+            "location", access=py_trees.common.Access.READ)
+        self.blackboard.register_key(
+            "location", access=py_trees.common.Access.WRITE)
+        
+        
+    def setup(self):
+        self.logger.debug("  %s [SeeAruco::setup()]" % self.name)
+        # service used to set goal of move behaviour
+
+        rospy.wait_for_service('/marker_pose')
+
+        try:
+            self.server_get_goal = rospy.ServiceProxy(
+                '/marker_pose', MarkerPose)            
+            self.logger.debug(
+                "  %s [SeeAruco::setup() Server connected!]" % self.name)
+        except rospy.ServiceException as e:
+            self.logger.debug("  %s [SeeAruco::setup() ERROR!]" % self.name)
+
+    def initialise(self):
+        self.logger.debug("  %s [SeeAruco::initialise()]" % self.name)
+
+    def update(self):
+
+        self.logger.debug("  {}: call service /server_get_goal".format(self.name))
+
+        
+
+        resp = self.server_get_goal(True).pose_msg
+
+        curr_z = resp.pose.position.z
+        print('z pos: ', curr_z)
+
+        if curr_z == 0:
+            return py_trees.common.Status.RUNNING
+        else:
+            self.blackboard.location = resp
+            return py_trees.common.Status.SUCCESS
+
+    def terminate(self, new_status):
+        # Call the service to turn on the pump (argument: True)
+        self.logger.debug("  %s [SeeAruco::terminate().terminate()][%s->%s]" %
+                          (self.name, self.status, new_status))
+
 if __name__ == "__main__":
     py_trees.logging.level = py_trees.logging.Level.DEBUG
     rospy.init_node("behavior_trees")
     # print("nonooooo")
 
     # # Create Behaviors
-    go_pick = GoToPoint("go_pick",'pick',[0.2,0.2,-0.1,0,0,0])
+    # go_pick = GoToPoint("go_pick",'pick',[0.2,0.2,-0.1,0,0,0])
     
-    go_up1 = GoToPoint("go_up",'',[0.2,0.2,-0.2,0,0,0])
+    # go_up1 = GoToPoint("go_up",'',[0.2,0.2,-0.2,0,0,0])
 
-    go_place = GoToPoint("go_place",'pick',[-0.3,-0.3,-0.1,0,0,0])
+    # go_place = GoToPoint("go_place",'pick',[-0.3,-0.3,-0.1,0,0,0])
     
-    go_up2 = GoToPoint("go_up",'',[-0.3,-0.3,-0.2,0,0,0])
+    # go_up2 = GoToPoint("go_up",'',[-0.3,-0.3,-0.2,0,0,0])
+
+    see_aruco = SeeAruco('see_aruco')
+    go_pick = GoToPoint("go_pick",'pick')
 
     # Create Behavior Tree
     root = py_trees.composites.Sequence(name="Mobile Pick n Place", memory=True)
 
+    root.add_children([see_aruco, go_pick])
+
     # Add behaviors to the tree
-    root.add_children([go_pick, go_up1, go_place, go_up2])
+    # root.add_children([go_pick, go_up1, go_place, go_up2])
 
     # Display the tree before executing it
     py_trees.display.render_dot_tree(root)
